535ef8f59a7ed348517a7011f51ce9f836628652
eduardo-nedel <emnedel@inf.ufsm.br>
2018-05-13 00:05:47 -0300

    Consertar os commits

--- T1/trabalho.cpp
+++ T1/trabalho.cpp
@@ -2,35 +2,46 @@
 #include <fstream>
 #include <string>
 #include <vector>
- 
+#include <algorithm>
+
+
 using namespace std;
- 
+
+int num_char(vector <string> palavras) {
+    int cont = 0, j = 0;
+    for (int i = 0; i < palavras.size(); ++i){
+        for (j = 0; j < palavras[i].size(); ++j)
+        cont +=j;
+    }
+    return cont;
+}
+
 int main()
 {
     int num_bites = 0;
     cout << "Digite o numero de bits que voce deseja ter em memoria" << endl;
     cin >> num_bites;
-   
-   
+    
+    
     ifstream arqEntrada ("entrada1.txt");
- 
+
     if (!arqEntrada.is_open()){
         cout << "Erro ao ler o Arquivo" << endl;
     }
     else {
- 
+
         string str;
-        string palavras;
+        vector <string> palavras;
         bool continuar = true;
         int num_arq = 0;
         char saida;
- 
+
         // separa os blocos em arquivos
         while (!arqEntrada.eof()) {
             arqEntrada >> saida >> noskipws;
- 
+
             // separa em palavras
-            while (saida >= '!' && saida <= '~' && continuar) {
+            while (((tolower(saida) >= 'a' && tolower(saida) <= 'z') || (saida >= '0' && saida <= '9')) && continuar) {
                 str.push_back(saida);
                 arqEntrada >> saida >> noskipws;
                 if(str.size() >= num_bites) {
@@ -38,68 +49,72 @@ int main()
                     continuar = false;
                 }
             }
- 
+
             if (continuar && str.size() > 0){
-                if (str.size() + palavras.size() > num_bites){
-                    //cria arq
+                if (str.size() + num_char(palavras) > num_bites){
+                    //cria um arquivo
                     num_arq+=1;
                     string aux = "Temp";
                     aux += to_string(num_arq);
                     aux += ".txt";
                     ofstream mandar (aux);
- 
-                    //manda palavras
-                    mandar << palavras;
+
+                    //manda para um arquivo as palavras
+                    sort(palavras.begin(), palavras.end());
+                    for (int i = 0; i < palavras.size(); ++i) 
+                        mandar << palavras[i];
                     mandar.close();
- 
-                    //erase palavras
+
+                    //apaganos as palavras
                     palavras.clear();
- 
-                    //insert
-                    palavras.insert(palavras.end(), str.begin(), str.end());
-                    if (palavras.size() < num_bites)
-                        palavras.push_back(' ');
+
+                    //alimentamos o vector de palavras
+                    if(str.size() < num_bites-1)
+                        str.push_back(' ');
+                    palavras.push_back(str);
                     str.clear();
                 }
-                else if (str.size() + palavras.size() == num_bites){
-                    //cria arq
+                else if (str.size() + num_char(palavras) == num_bites){
+                    //cria um arquivo
                     num_arq+=1;
                     string aux = "Temp";
                     aux += to_string(num_arq);
                     aux += ".txt";
                     ofstream mandar (aux);
- 
-                    //insert
-                    palavras.insert(palavras.end(), str.begin(), str.end());
-                    if (palavras.size() < num_bites)
-                        palavras.push_back(' ');
+
+                    //alimentamos o vector de palavras
+                    if(str.size() < num_bites-1)
+                        str.push_back(' ');
+                    palavras.push_back(str);
                     str.clear();
- 
-                    //manda palavras
-                    mandar << palavras;
+
+                    //manda para um arquivo as palavras
+                    sort(palavras.begin(), palavras.end());
+                    for (int i = 0; i < palavras.size(); ++i)
+                        mandar << palavras[i];
                     mandar.close();
- 
-                    //erase palavras
+
+                    //apaganos as palavras
                     palavras.clear();
                 }
                 else {
-                    //insert
-                    palavras.insert(palavras.end(), str.begin(), str.end());
-                    if (palavras.size() < num_bites)
-                        palavras.push_back(' ');
+                    //alimentamos o vector de palavras
+                    if(str.size() < num_bites-1)
+                        str.push_back(' ');
+                    palavras.push_back(str);
                     str.clear();
                 }
             }
             continuar = true;
         }
     }
- 
- 
- 
+
+
+
     arqEntrada.close();
- 
+
     return 0;
 }
- 
+
 // remove("arquivo.txt");
 // criar uma string onde concatena os nomes
